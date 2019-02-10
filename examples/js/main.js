ac403933e69223ad3ad92d8015dcdca39396ed3b
Peter <bjfubjfu@gmail.com>
2019-02-10 11:43:35 +0100

    Update main.js

--- examples/js/main.js
+++ examples/js/main.js
@@ -39,6 +39,8 @@ if(developMode){
 }
 
 if (typeof Cesium !== "undefined" && typeof echarts !== "undefined" && typeof CesiumHeatmap !== "undefined") {
+	// personal key, you can apply your own key from Bing Map
+        Cesium.BingMapsApi.defaultKey = "AlgsUaY9R3p9MHmp-GN-o4j9HsdPrxboqvpaWwL5Da5vwQv7YkdC426vW1s6Lxrk";
     onload(Cesium,CesiumHeatmap,echarts);
 } else if (typeof require === "function") {
     require(["Cesium","CesiumHeatmap","echarts"], onload);
